14107930c52d947b37bed0ec3c64280b277f4dc9
Zorii Bohdan <bodkazorii@gmail.com>
2024-06-30 13:33:42 +0300

    better annotation selection

--- src/main/java/mate/zorii/bookstore/dto/BookDto.java
+++ src/main/java/mate/zorii/bookstore/dto/BookDto.java
@@ -1,10 +1,7 @@
 package mate.zorii.bookstore.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -21,7 +18,7 @@ public class BookDto {
     @Pattern(regexp = "^(97(8|9))?\\d{9}(\\d|X)$", message = "Invalid ISBN format")
     private String isbn;
     @NotNull
-    @Min(0)
+    @Positive
     private BigDecimal price;
     @Size(max = 2000, message = "Description must be less than 2000 characters")
     private String description;
