eabf352d0f9a62b4904baa493882b6d95721fd01
Zorii Bohdan <bodkazorii@gmail.com>
2024-06-30 12:38:56 +0300

    add GlobalExceptionHandler class

---
+++ src/main/java/mate/zorii/bookstore/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package mate.zorii.bookstore.exception;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getAllErrors().stream()
+                .map(this::getErrorMessage)
+                .toList();
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    private String getErrorMessage(ObjectError objectError) {
+        if (objectError instanceof FieldError fieldError) {
+            return fieldError.getField() + " " + fieldError.getDefaultMessage();
+        }
+        return objectError.getDefaultMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerExceptions(NullPointerException ex) {
+        return new ResponseEntity<>("Null pointer exception occurred",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
