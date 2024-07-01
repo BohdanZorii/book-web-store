998a1fe528688f0e0b1ba90981f841ba6c872ebc
Zorii Bohdan <bodkazorii@gmail.com>
2024-07-01 10:34:34 +0300

    add pagination

--- src/main/java/mate/zorii/bookstore/controller/BookController.java
+++ src/main/java/mate/zorii/bookstore/controller/BookController.java
@@ -8,6 +8,8 @@ import mate.zorii.bookstore.dto.BookDto;
 import mate.zorii.bookstore.dto.BookSearchRequestDto;
 import mate.zorii.bookstore.dto.CreateOrUpdateBookRequestDto;
 import mate.zorii.bookstore.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +26,8 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> findAll() {
-        return bookService.findAll();
+    public Page<BookDto> findAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
