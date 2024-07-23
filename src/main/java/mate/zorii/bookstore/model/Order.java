0206e6446fc063e0d19ff3999eb60e439cca5762
Zorii Bohdan <bodkazorii@gmail.com>
2024-07-23 10:19:55 +0300

    fix checkstyle

--- src/main/java/mate/zorii/bookstore/model/Order.java
+++ src/main/java/mate/zorii/bookstore/model/Order.java
@@ -1,13 +1,24 @@
 package mate.zorii.bookstore.model;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "orders")
@@ -32,7 +43,7 @@ public class Order {
     @OneToMany(mappedBy = "order", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<OrderItem> orderItems = new HashSet<>();
 
-    public enum Status{
+    public enum Status {
         PENDING,
         CONFIRMED,
         SHIPPED,
